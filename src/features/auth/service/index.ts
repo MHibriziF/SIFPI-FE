2ed852c19827505fd618e9ef448df0d67db5b3f3
Theo Ananda Lemuel <theo.ananda@ui.ac.id>
2026-03-03 20:27:33 +0700

    feat( auth ): logout

--- src/features/auth/service/index.ts
+++ src/features/auth/service/index.ts
@@ -4,3 +4,7 @@ import type { LoginRequest, AuthResponse } from '@/features/auth/types';
 export async function login(data: LoginRequest) {
   return apiPost<AuthResponse>('api/auth/login', data);
 }
+
+export async function logout() {
+  return apiPost<null>('/api/auth/logout');
+}
